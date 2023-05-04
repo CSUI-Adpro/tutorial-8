7f4a44a3bdcd348c1975a2c993101153e8f4304c
eugeniusms <eugeniusms@gmail.com>
2023-05-04 12:48:02 +0700

    Article Service Done

--- article-service/src/main/java/id/ac/ui/cs/advprog/articleservice/service/ArticleServiceImpl.java
+++ article-service/src/main/java/id/ac/ui/cs/advprog/articleservice/service/ArticleServiceImpl.java
@@ -18,24 +18,36 @@ public class ArticleServiceImpl implements ArticleService{
 
     @Override
     public List<Article> getAllArticles() {
-        // TODO: Lengkapi kode berikut
-        return null;
+        // TODO DONE: Mengembalikan semua Article yang ada di database
+        List<Article> articles = null;
+        articles = articleRepository.findAll();
+        return articles;
     }
 
     @Override
     public Article findArticleById(Integer id) {
-        // TODO: Lengkapi kode berikut
-        return null;
+        // TODO DONE: Mengembalikan Article dengan id yang ditentukan
+        Article article = null;
+        article = articleRepository.findArticleById(id);
+        return article;
     }
 
     @Override
     public Article createArticle(CreateArticleDTO request) {
-        // TODO: Lengkapi kode berikut
-        return null;
+        // TODO DONE: Membuat Article baru dengan data yang ditentukan
+        Article article = null;
+        article = new Article();
+        article.setAuthor(request.getAuthor());
+        article.setTitle(request.getTitle());
+        article.setContent(request.getContent());
+        article.setTimestamp(Timestamp.valueOf(LocalDateTime.now()));
+        articleRepository.save(article);
+        return article;
     }
 
     @Override
     public void deleteArticle(Integer id) {
-        // TODO: Lengkapi kode berikut
+        // TODO DONE: Menghapus Article dengan id yang ditentukan
+        articleRepository.deleteById(id);
     }
 }
